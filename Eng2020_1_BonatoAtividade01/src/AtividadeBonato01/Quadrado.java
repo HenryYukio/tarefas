5334fea0fb7a3802fb44949aacd138aa1624f6d9
Regina Giovannini <52223538+ReGiovannini@users.noreply.github.com>
2020-03-11 15:46:30 -0300

    Add files via upload

---
+++ Eng2020_1_BonatoAtividade01/src/AtividadeBonato01/Quadrado.java
@@ -0,0 +1,25 @@
+package AtividadeBonato01;
+
+public class Quadrado extends Poligono implements Diagonal {
+
+	public Quadrado(double lado) {
+		super(lado, lado);
+	}
+	
+	
+	@Override
+	public double diagonal() {
+		return getAltura()*Math.sqrt(2);
+	}
+
+	@Override
+	public double area() {
+		return getAltura()*getBase();
+	}
+	
+	@Override
+	public double perimetro() {
+		return getBase() + getBase() + getBase() + getBase();
+	}
+		
+}
